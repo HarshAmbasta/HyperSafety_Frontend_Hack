3ea463adff9b28a5358c6f50fdc67501c5ca2e44
HarshAmbasta <harshambasta01@gmail.com>
2022-01-29 00:25:13 +0530

    Empolyee Management UI for Cross-Platform Collaboration

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: unused_import, deprecated_member_use, prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:hypersafety_frontend_hack/Screens/Home_Screen.dart';
+import 'package:hypersafety_frontend_hack/Screens/Add_Employee.dart';
+import 'package:hypersafety_frontend_hack/Screens/Delete_Employee.dart';
+import 'package:hypersafety_frontend_hack/Screens/Employee_Records.dart';
+import 'package:hypersafety_frontend_hack/Screens/Reset_Record.dart';
+
+// flutter run --no-sound-null-safety
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HyperSafety-Frontend-Hack',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.white, //Color(0xff1d2021), Color of Main Screen.
+        accentColor: Color(0xFF282828),
+        fontFamily: 'FiraCode',
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Color(0xff32302f), //Colors.white,
+              displayColor: Colors.white, //Color(0xff32302f),
+            ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
